013c46671e10bb807f5f83511b65754321265e4f
MadeInPierre <pielaclau@gmail.com>
2023-03-07 10:15:41 +0100

    test(dryrun): add a trivial run test of the demo

---
+++ tests/test_dryrun.py
@@ -0,0 +1,18 @@
+import os
+
+import pytest
+
+
+# Set dummy environment variables for credentials to bypass manual input
+@pytest.fixture(autouse=True, scope="session")
+def set_credentials_env():
+    os.environ["FINARY_EMAIL"] = "test_email"
+    os.environ["FINARY_PASSWORD"] = "test_password"
+    yield
+    os.environ.pop("FINARY_EMAIL")
+    os.environ.pop("FINARY_PASSWORD")
+
+
+# Simply run the demo and pass if there was no error (e.g. missing dependencies)
+def test_dryrun():
+    os.system("python3 examples/demo.py")

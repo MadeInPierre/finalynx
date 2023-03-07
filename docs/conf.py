b5f770b4ff6a20a8be0904d2fa1555e24d23f2db
MadeInPierre <pielaclau@gmail.com>
2023-03-07 18:03:56 +0100

    docs: set theme to rtd

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,8 @@ import sys
 
 sys.path.insert(0, os.path.abspath(".."))
 
+import finalynx  # noqa: F401, E402
+
 
 # -- Project information -----------------------------------------------------
 
@@ -35,7 +37,8 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "myst_parser",
-    "sphinx_material",
+    # "sphinx_material",
+    "sphinx_rtd_theme",
 ]
 
 # Add support for Markdown files
@@ -55,28 +58,29 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_material"
+# html_theme = "sphinx_material"
+html_theme = "sphinx_rtd_theme"
 
 # Material theme options (see theme.conf for more information)
-html_theme_options = {
-    # Set the name of the project to appear in the navigation.
-    "nav_title": "Finalynx",
-    # Specify a base_url used to generate sitemap.xml. If not
-    # specified, then no sitemap will be built.
-    # 'base_url': 'https://project.github.io/project',
-    # Set the color and the accent color
-    "color_primary": "blue",
-    "color_accent": "light-blue",
-    # Set the repo location to get a badge with stats
-    "repo_url": "https://github.com/MadeInPierre/finalynx/",
-    "repo_name": "finalynx",
-    # Visible levels of the global TOC; -1 means unlimited
-    "globaltoc_depth": 3,
-    # If False, expand all TOC entries
-    "globaltoc_collapse": False,
-    # If True, show hidden TOC entries
-    "globaltoc_includehidden": False,
-}
+# html_theme_options = {
+#     # Set the name of the project to appear in the navigation.
+#     "nav_title": "Finalynx",
+#     # Specify a base_url used to generate sitemap.xml. If not
+#     # specified, then no sitemap will be built.
+#     # 'base_url': 'https://project.github.io/project',
+#     # Set the color and the accent color
+#     "color_primary": "blue",
+#     "color_accent": "light-blue",
+#     # Set the repo location to get a badge with stats
+#     "repo_url": "https://github.com/MadeInPierre/finalynx/",
+#     "repo_name": "finalynx",
+#     # Visible levels of the global TOC; -1 means unlimited
+#     "globaltoc_depth": 3,
+#     # If False, expand all TOC entries
+#     "globaltoc_collapse": False,
+#     # If True, show hidden TOC entries
+#     "globaltoc_includehidden": False,
+# }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

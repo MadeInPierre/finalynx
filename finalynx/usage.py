f167f672b40a2472aace5a0bb3596f4dd01ae07d
Pierre Laclau <pielaclau@gmail.com>
2023-03-17 17:45:27 +0100

    feat(fetch): can now cache data locally, defaults to 1h (#36)
    * feat(fetch): data is now cached locally (auto-fetch after 1h)
    
    * refactor(fetch): fetch is now in a class, added clear cache option
    
    * refactor(fetch): file cleanup, add docstrings

--- finalynx/usage.py
+++ finalynx/usage.py
@@ -28,7 +28,8 @@ Options:
   -v --version         Display the current version and exit
 {main_filter(main_options)}
   -i --ignore-orphans  Ignore fetched lines that you didn't reference in your portfolio
-  -f --force-signin    Sign in to Finary even if there is an existing cookies file
+  -c --clear-cache     Delete any data from Finary that was cached locally
+  -f --force-signin    Clear cache, cookies and credentials files to sign in again
   -a --hide-amounts    Display your portfolio with dots instead of the real values (easier to share)
   -r --hide-root       Display your portfolio without the root (cosmetic preference)
 

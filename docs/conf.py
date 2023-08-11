2316fdd2dcee5846f6cf6af5890fc9074e88cc33
MadeInPierre <pielaclau@gmail.com>
2023-08-11 13:34:36 +0200

    chore: manual bump to version 1.22.2

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2023, Pierre Laclau"
 author = "Pierre Laclau"
 
 # The full version, including alpha/beta/rc tags
-release = "1.21.0"
+release = "1.22.2"
 
 
 # -- General configuration ---------------------------------------------------
